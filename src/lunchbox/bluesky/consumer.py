47c554e96d21a6f9f78c147067b90e2f77cad010
Rose Yemelyanova <rose.yemelyanova@diamond.ac.uk>
2023-05-19 15:38:59 +0100

    made bluesky docs be consumed by a spot finding algorithm

--- src/lunchbox/bluesky/consumer.py
+++ src/lunchbox/bluesky/consumer.py
@@ -1,5 +1,7 @@
 from pathlib import Path
-from typing import Any, Callable, Dict, Optional
+from typing import Any, Dict, Optional
+
+from lunchbox.analysis.spotfinder import SpotFinder
 
 
 class Consumer:
@@ -8,21 +10,35 @@ class Consumer:
     Stores information about resource and datum ids to keep track of files.
     """
 
-    def __init__(self, executable: Callable[..., None]) -> None:
-        self.executable: Callable[..., None] = executable
+    def __init__(self) -> None:
+        self.spot_finder: Optional[SpotFinder] = None
         self.resource_id: Optional[str] = None
         self.datum_id: Optional[str] = None
         self.datum_kwargs: Optional[Dict[str, Any]] = None
         self.filepath: Optional[Path] = None
+        self.pixel_width: Optional[float] = None
+        self.grating_distance: Optional[float] = None
 
     def __call__(self, name: str, doc: Dict[str, Any]) -> None:
         # if it's a resource doc, get the id...
+        if name == "descriptor":
+            config = doc["configuration"]["lunchbox"]["data"]
+
+            assert config, "wrong document schema"
+            self.pixel_width = float(config["lunchbox_pixel_width"])
+            self.grating_distance = float(config["lunchbox_grating_distance"])
+
         if name == "resource":
             root = str(doc.get("root"))
             filename = str(doc.get("resource_path"))
+            filepath = Path(root) / Path(filename)
 
-            self.filepath = Path(root) / filename
             self.resource_id = doc.get("uid")
+
+            assert self.pixel_width and self.grating_distance, "descriptor not found"
+            self.spot_finder = SpotFinder(
+                filepath, self.pixel_width, self.grating_distance
+            )
 
         elif name == "datum":
             if doc["resource"] == self.resource_id:
@@ -31,10 +47,11 @@ class Consumer:
 
         elif name == "event":
             data: Optional[Dict[str, Any]] = doc.get("data")
-
+            assert self.spot_finder
             if data and data.get("value") == self.datum_id:
                 kwargs: Dict[str, Any] = (
                     self.datum_kwargs if self.datum_kwargs is not None else {}
                 )
-
-                self.executable(**kwargs)
+                angle = data.get("LUNCHBOX:SERVO:RBV")
+                if angle:
+                    self.spot_finder(float(angle), **kwargs)
--- src/lunchbox/bluesky/plan.py
+++ src/lunchbox/bluesky/plan.py
@@ -1,10 +1,12 @@
 from pprint import pprint
 
 import bluesky.plan_stubs as bps
+import matplotlib.pyplot as plt
 from bluesky.plans import scan
 from bluesky.run_engine import RunEngine
 from ophyd import EpicsSignal
 
+from lunchbox.bluesky import Consumer
 from lunchbox.devices.lunchbox import LunchBox
 
 lunchbox = LunchBox(name="lunchbox")
@@ -22,5 +24,21 @@ def scan_diffraction():
 
 
 if __name__ == "__main__":
+    consumer = Consumer()
+
     RE = RunEngine()
+    RE.subscribe(consumer)
     RE(scan_diffraction(), lambda name, doc: pprint({"name": name, "doc": doc}))
+
+    spot_finder = consumer.spot_finder
+    assert spot_finder
+    plus_len = len(spot_finder.plus_angles)
+    minus_len = len(spot_finder.minus_angles)
+    plt.plot(spot_finder.servo_angles[:plus_len], spot_finder.plus_angles, label="+")
+    plt.plot(spot_finder.servo_angles[:minus_len], spot_finder.minus_angles, label="-")
+
+    plt.xlabel(r"$\alpha$ (degrees)")
+    plt.ylabel(r"$\theta$ (degrees)")
+
+    plt.legend(loc="upper left")
+    plt.show()
